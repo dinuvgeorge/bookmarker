b9282a376d4b4a4fb44cae397f4e6e1ae6a382e4
George Dinu <george.dinu@simpology.com>
2024-12-12 10:20:31 +0200

    Config angular-in-memory-web-api + config UUID for uniqueID's

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,17 @@ import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { WmatToolbarComponent } from 'wmat-components';
 import { BookmarkService } from './services/bookmark.service';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, WmatToolbarComponent],
+  imports: [RouterOutlet, WmatToolbarComponent, AsyncPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   bookmarkService = inject(BookmarkService);
+
+  bookmarks = this.bookmarkService.getItems();
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,27 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  importProvidersFrom,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideStore } from '@ngrx/store';
-import { reducers, metaReducers } from './reducers';
+import { metaReducers, reducers } from './reducers';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataBookmarkService } from './a-inmemory-data-services/in-memory-data-bookmark.service';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync('noop'), provideStore(reducers, { metaReducers })]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHttpClient(),
+    importProvidersFrom(
+      HttpClientInMemoryWebApiModule.forRoot(InMemoryDataBookmarkService),
+    ),
+    provideAnimationsAsync('noop'),
+    provideStore(reducers, { metaReducers }),
+  ],
 };
